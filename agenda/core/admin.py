d9c95aaa08c354f2dabdab4fb3293505a7ccfdab
Afonso Abreu <afonsohenrique2@hotmail.com>
2022-01-20 14:01:22 -0300

    Aula 3 - Criação de tabela Evento no BD

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Evento
+
+#Personalizar a vizualização da tabela no portal Admin
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento','data_criacao') #Ordem e colunas da classe que irão aparecer no portal Admin
+    list_filter = ('usuario','data_evento',) #Criar filtro no Admin (tem que ter virgula no final para não dar erro)
 
 # Register your models here.
+#Para registrar 'Evento' no portal Admin
+admin.site.register(Evento, EventoAdmin) #Associando Evento a EventoAdmin
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#Criar arquivo de migração antes de enviar para BD (pasta migrations):
+# --> python manage.py makemigrations core
+#Criar comando SQL antes de enviar para BD:
+# --> python manage.py sqlmigrate core 0001
+#Migrar models para o BD (de um módulo especifico e uma model especifica):
+# --> python manage.py migrate core 0001
+
+#Python cria a primary key automaticamente
+#Se não for especificado um nome pra tabela, o Python cria o nome padrão 'modulo_model'
+
+#Para importar no Admin, ir para arquivo admin.py
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100) #string com no maximo 100 caracteres
+    descricao = models.TextField(blank=True, null=True) #campo de texto que pode ser vazio ou nulo
+    data_evento = models.DateTimeField(verbose_name= 'Data do Evento') #Campo de data que não pode ser nulo.
+                                        #verbose_name --> nome personalizado para o portal Admin
+    data_criacao = models.DateTimeField(auto_now=True) #Campo de data com datatime atual como default
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #Criar foreignkey e ativar Delete on Cascade
+
+    class Meta: #Forçar metadados da tabela
+        db_table = 'evento' #Mudar nome da tabela para 'evento'
+
+    def __str__(self): #Nome que irá aparecer para cada registro na tabela no Admin
+        return self.titulo
